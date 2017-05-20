a5234e83921d97e5fbd4a73a034944aca08fed14
DNepovim <jsem@dominikblaha.cz>
2017-05-20 13:13:09 +0200

    Add sidebar

--- theme/functions.php
+++ theme/functions.php
@@ -1,3 +1,17 @@
 <?php
 
 require __DIR__ . '/theme-init.php';
+
+function getMenuItems($menuName) {
+	$locations = get_nav_menu_locations();
+	if (isset($locations[$menuName])) {
+		$menu = get_term($locations[$menuName], 'nav_menu');
+		return wp_get_nav_menu_items($menu->term_id);
+	}
+	return [];
+}
+
+add_theme_support('menus');
+register_nav_menu('main_menu', 'Hlavní menu');
+
+$View->main_menu = getMenuItems('main_menu');
